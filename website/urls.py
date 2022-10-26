648760d0f66b4e7e366ab1a306dfc58dd668b6c8
Suraj Prasan Das <114937033+nirAdhi@users.noreply.github.com>
2022-10-26 20:29:22 +0100

    Add files via upload

---
+++ website/urls.py
@@ -0,0 +1,24 @@
+"""website URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLConf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.urls import include, re_path
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    re_path(r'^$', views.index_redirect, name='index_redirect'),
+    re_path(r'^web/', include('web.urls')),
+    re_path(r'^admin/', admin.site.urls),
+]
